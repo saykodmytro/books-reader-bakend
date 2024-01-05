4a2b43d264bdc1f132743378f58cd534a346bc4c
Dmytro Sayko <saykodmytro@icloud.com>
2024-01-06 01:48:41 +0200

    up

--- server.js
+++ server.js
@@ -3,14 +3,16 @@ const mongoose = require("mongoose");
 const DB_HOST =
   "mongodb+srv://Dmytro:zCK1EBuXxgOdlX1L@cluster0.o4hocu8.mongodb.net/books_reader?retryWrites=true&w=majority";
 
-const app = require("./app");
-
 mongoose.set("strictQuery", true);
+
+const app = require("./app");
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000);
+    app.listen(3000, () => {
+      console.log("Serv Start");
+    });
   })
   .catch((error) => {
     console.log(error.message);
